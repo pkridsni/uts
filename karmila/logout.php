55d9eb893064d7acf2da032ad2de1311f7d3287d
Fikri <if23.muhammadf@mhsubpkarawang.ac.id>
2024-11-15 01:30:58 +0700

    First commit

---
+++ karmila/logout.php
@@ -0,0 +1,18 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION["akun-admin"]) && !isset($_SESSION["akun-user"])) {
+    header("Location: login.php");
+    exit;
+} 
+
+session_unset();
+session_destroy();
+
+?>
+
+<script>
+    alert('Berhasil Logout!');
+    location.href = '../index.html'; // Mengarahkan ke index.html di luar folder
+</script>
